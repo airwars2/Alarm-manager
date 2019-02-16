d5a7314fc2ee97d8141781588862bf0ae51a516a
airwars2 <47667163+airwars2@users.noreply.github.com>
2019-02-16 15:04:55 +0530

    Add files via upload

---
+++ BasicXML/app/src/main/java/nareshit/com/basicxml/BasicXmlActivity.java
@@ -0,0 +1,17 @@
+package nareshit.com.basicxml;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by nareshit on 6/6/2017.
+ */
+
+public class BasicXmlActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_basic);
+    }
+}
